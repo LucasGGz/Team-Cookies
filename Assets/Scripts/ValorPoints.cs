a3f9cf6d704a27841b451f16ce79fa90e33eb9c5
LucasGGz <112144033+LucasGGz@users.noreply.github.com>
2023-10-12 20:45:28 -0300

    Win condition

--- Assets/Scripts/ValorPoints.cs
+++ Assets/Scripts/ValorPoints.cs
@@ -7,19 +7,30 @@ public class ValorPoints : MonoBehaviour
 {
     public float puntos;
     public Text textoPuntos;
+    [SerializeField]
+    private GameObject youwintext; //texto que mostraremos
+    public MovimientoPersonaje movimientoPersonaje;
+
+    private void Awake()
+    {
+        youwintext.SetActive(false); //hace que el texto no se vea al inicio      
+    }
     void Start()
     {
-        puntos = 0f; 
+        puntos = 0f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        textoPuntos.text = ""+puntos.ToString();
+        textoPuntos.text = "" + puntos.ToString();
 
-        if (puntos == 2)
+        if (puntos == 1)
         {
             Debug.Log("Ganaste");
+            youwintext.SetActive(true);
+            movimientoPersonaje.speed = 0;
         }
     }
 }
